eec2ccd1f3305c5014ae9d01e2a985022df57e5e
Schedarian <75801123+Schedarian@users.noreply.github.com>
2023-08-08 19:25:09 +0300

    Update say.rb

--- Acropolis-Bot/src/commands/say.rb
+++ Acropolis-Bot/src/commands/say.rb
@@ -1,6 +1,6 @@
 Command_Say = lambda { |vars|
   if vars[:config][:register_commands?]
-    vars[:bot].register_application_command(:say, "Сообщение от имени бота, только для модераторов/администраторов", server_id: vars[:config][:server_id]) { |cmd|
+    vars[:bot].register_application_command(:say, "Сообщение от имени бота. Только для администраторов", server_id: vars[:config][:server_id]) { |cmd|
       cmd.string(:text, "Текст сообщения", required: true)
     }
   end
@@ -8,7 +8,7 @@ Command_Say = lambda { |vars|
   vars[:bot].application_command(:say) { |handler|
     handler.defer(ephemeral: true)
 
-    if vars[:bot].member(handler.server_id, handler.user.id).permission?(vars[:config][:moderator_commands_allowed].to_sym) == false
+    if vars[:bot].member(handler.server_id, handler.user.id).permission?(:administrator) == false
       handler.send_message(content: "У вас нет прав на использование данной команды")
     else
       begin
