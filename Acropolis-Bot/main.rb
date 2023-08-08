6c24e6cd981432db799b3790d21fec9a82976d0a
Schedarian <75801123+Schedarian@users.noreply.github.com>
2023-08-08 19:21:42 +0300

    Update main.rb

--- Acropolis-Bot/main.rb
+++ Acropolis-Bot/main.rb
@@ -6,16 +6,13 @@ require "bundler/setup"
 Dir["./src/*.rb"].each { |file| require file }
 
 config = YAML.load(File.read("config.yaml"), symbolize_names: true)
-database = Database.new
-logger = MessageLogger.new(config)
-parser = Parser.new(config[:api_port])
 
 bot = Discordrb::Bot.new(token: config[:bot_token], intents: :all)
 bot.init_cache
 bot.ready {
   bot.idle
   loop {
-    bot.playing = "Mindustry 2"
+    bot.playing = "ping for help"
     sleep(3600 * 6)
   }
 }
@@ -25,9 +22,10 @@ vars = {
   :bot => bot,
   :config => config,
   :utils => Utils,
-  :database => database,
-  :logger => logger,
-  :parser => parser,
+  :database => Database.new,
+  :logger => MessageLogger.new(config),
+  :parser => Parser.new(config[:api_port]),
+  :timeoutmanager => TimeoutManager.new(bot, config),
 }
 
 Thread.new {
@@ -35,5 +33,7 @@ Thread.new {
   EventHandler.init_events(vars)
   CommandHandler.init_commands(vars)
 }
+
+#bot.get_application_commands(server_id: vars[:config][:server_id]).each { |cmd| bot.delete_application_command(cmd.id, server_id: vars[:config][:server_id]) }
 
 bot.run
